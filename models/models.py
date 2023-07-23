d21bb5dae711b554c81374195f5acab19b09de36
yuki-younai <1401261749@qq.com>
2023-07-23 09:10:49 +0800

    ADD MOE BASE

--- models/models.py
+++ models/models.py
@@ -126,6 +126,35 @@ class DGRec(BaseGraphModel):
 class BasetestRec(BaseGraphModel):
     def __init__(self, args, dataloader):
         super(BasetestRec, self).__init__(args, dataloader)
+
+        self.attention_experts=TargetAttention(args)  
+    def build_layer(self, idx):
+        return BasetestLayer(self.args)
+
+    def get_embedding(self):
+        user_embed = [self.user_embedding]
+        item_embed = [self.item_embedding]
+        h = self.node_features
+
+        for layer in self.layers:
+
+            h_item = layer(self.graph, h, ('user', 'rate', 'item'))
+            h_user,muti_int = layer(self.graph, h, ('item', 'rated by', 'user'))
+            h_user=self.attention_experts(muti_int)
+            #h_user=muti_int.sum(dim=1)
+            h = {'user': h_user, 'item': h_item}
+
+        
+        h = {'user': h_user, 'item': h_item}
+
+        return h
+
+
+
+
+class MOERec(BaseGraphModel):
+    def __init__(self, args, dataloader):
+        super(BasetestRec, self).__init__(args, dataloader)
         self.k=args.k_experts
         self.n_experts=args.n_experts
         self.noisy_gating=True
@@ -226,127 +255,6 @@ class BasetestRec(BaseGraphModel):
         h = {'user': expert_outputs, 'item': h_item}
 
         return h,loss
-
-
-
-
-class MOERec(BaseGraphModel):
-    def __init__(self, args, dataloader):
-        super(MOERec, self).__init__(args, dataloader)
-        self.k=args.k_experts
-        self.n_experts=args.n_experts
-        self.noisy_gating=True
-        self.w_gate = nn.Parameter(torch.zeros(args.embed_size*args.num_context_codes, self.n_experts), requires_grad=True)#20000 5
-        self.w_noise = nn.Parameter(torch.zeros(args.embed_size*args.num_context_codes, self.n_experts), requires_grad=True)#20000 5
-        self.softplus = nn.Softplus()
-        self.softmax=nn.Softmax(dim=1)
-        self.register_buffer("mean", torch.tensor([0.0]))
-        self.register_buffer("std", torch.tensor([1.0]))
-        self.attention_experts=TargetAttention(args)
-        
-        
-    def _gates_to_load(self, gates):
-     
-        return (gates > 0).sum(0)
-
-    def _prob_in_top_k(self, clean_values, noisy_values, noise_stddev, noisy_top_values):#32 5,32 5,32 5,32 3
-     
-        batch = clean_values.size(0)#32
-        m = noisy_top_values.size(1)#3
-        top_values_flat = noisy_top_values.flatten()#32*3
-
-        threshold_positions_if_in = torch.arange(batch, device=clean_values.device) * m + self.k
-        threshold_if_in = torch.unsqueeze(torch.gather(top_values_flat, 0, threshold_positions_if_in), 1)
-        is_in = torch.gt(noisy_values, threshold_if_in)
-        threshold_positions_if_out = threshold_positions_if_in - 1
-        threshold_if_out = torch.unsqueeze(torch.gather(top_values_flat, 0, threshold_positions_if_out), 1)
-        # is each value currently in the top k.
-        normal = Normal(self.mean, self.std)
-        prob_if_in = normal.cdf((clean_values - threshold_if_in)/noise_stddev)
-        prob_if_out = normal.cdf((clean_values - threshold_if_out)/noise_stddev)
-        prob = torch.where(is_in, prob_if_in, prob_if_out)
-        
-        
-        return prob
-
-    def noisy_top_k_gating(self, x, train, noise_epsilon=1e-2):#32 20000,true
-       
-        clean_logits = x @ self.w_gate##32 20000,20000 5-->32 5
-        if self.noisy_gating and train:
-            raw_noise_stddev = x @ self.w_noise##32 20000,20000 5-->32 5
-            noise_stddev = ((self.softplus(raw_noise_stddev) + noise_epsilon))#32 5
-            noisy_logits = clean_logits + (torch.randn_like(clean_logits) * noise_stddev)#32 5+(_rand(32 5)*32 5)
-            logits = noisy_logits
-        else:
-            logits = clean_logits
-
-        # calculate topk + 1 that will be needed for the noisy gates
-        top_logits, top_indices = logits.topk(min(self.k + 1, self.n_experts), dim=1)#32 3
-        top_k_logits = top_logits[:, :self.k]#32 2
-        top_k_indices = top_indices[:, :self.k]#32 2
-        top_k_gates = self.softmax(top_k_logits)#32 2
-
-        zeros = torch.zeros_like(logits, requires_grad=True)#32 5
-        gates = zeros.scatter(1, top_k_indices, top_k_gates)#32 5，每一行只有两个不为0的数，下标为排序后前2的下标，值为k_gates
-
-        if self.noisy_gating and self.k < self.n_experts and train:
-            load = (self._prob_in_top_k(clean_logits, noisy_logits, noise_stddev, top_logits)).sum(0)#32 5,32 5,32 5,32 3
-        else:
-            load = self._gates_to_load(gates)
-        return gates, load
-    def cv_squared(self, x):#1 5
-      
-        eps = 1e-10
-        # if only num_experts = 1
-
-        if x.shape[0] == 1:
-            return torch.tensor([0], device=x.device, dtype=x.dtype)
-        return x.float().var() / (x.float().mean()**2 + eps)    
-    def build_layer(self, idx):
-        return MOERecLayer(self.args)
-
-    def get_embedding(self):
-        user_embed = [self.user_embedding]
-        item_embed = [self.item_embedding]
-        h = self.node_features
-        
-        for layer in self.layers:
-
-            h_item = layer(self.graph, h, ('user', 'rate', 'item'))
-            muti_int,h_user = layer(self.graph, h, ('item', 'rated by', 'user'))
-            h = {'user': h_user, 'item': h_item}
-            
-           
-        
-        user_muti=torch.randn(0).to(muti_int.device)  
-        importance_sum=torch.randn(0).to(muti_int.device)  
-        load_sum=torch.randn(0).to(muti_int.device) 
-        for i in self.data_split:
-            batch=self.data[i:i+32]
-            muti=muti_int[batch]
-            gate,load=self.noisy_top_k_gating(muti.view(muti.shape[0],-1),True)
-            importance = gate.sum(0)
-            dispatcher = SparseDispatcher(self.n_experts, gate)
-            expert_inputs_user = dispatcher.dispatch(muti)
-        
-            expert_outputs=[]
-            for i in range(self.n_experts):
-                if len(expert_inputs_user[i])>0:
-                     expert_outputs.append(self.attention_experts[i](expert_inputs_user[i]))
-                else:
-                     kk=1    
-                    
-            expert_outputs=dispatcher.combine(expert_outputs) 
-            
-            user_muti=torch.cat([user_muti,expert_outputs],dim=0)
-            importance_sum=torch.cat([importance_sum,importance],dim=0)
-            load_sum=torch.cat([load_sum,load],dim=0)
-            
-        loss = self.cv_squared(importance_sum) + self.cv_squared(load_sum)
-        print(loss)
-        h_muti={'user':user_muti, 'item': h_item}
-                          
-        return h_muti,loss
                 
 class TargetAttention(nn.Module):
     def __init__(self, args):
